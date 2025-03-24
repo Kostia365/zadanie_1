0917d8f88cbc0eac55e3006fd286d40e84a70298
Kostia365 <90925004+Kostia365@users.noreply.github.com>
2025-03-24 01:01:39 +0100

    add all parts

---
+++ templatemo_591_villa_agency/parts/nav.php
@@ -0,0 +1,19 @@
+<nav class="main-nav">
+    <!-- ***** Logo Start ***** -->
+    <a href="index.html" class="logo">
+        <h1>Villa</h1>
+    </a>
+    <!-- ***** Logo End ***** -->
+    <!-- ***** Menu Start ***** -->
+    <ul class="nav">
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="properties.php">Properties</a></li>
+        <li><a href="property-details.php">Property Details</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
+    </ul>
+    <a class='menu-trigger'>
+        <span>Menu</span>
+    </a>
+    <!-- ***** Menu End ***** -->
+</nav>
